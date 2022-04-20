f2449c7903c1f2d96ccf3f1ae27e72b98bdf98ab
Bryanza Novirahman <bnrvideos@live.com>
2022-04-21 00:36:16 +0700

    Try to use provided template

---
+++ DrawBryLife.swiftpm/StoryView.swift
@@ -0,0 +1,43 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct StoryView: View {
+    
+    let story: Story
+    let pageIndex: Int
+    
+    var body: some View {
+        VStack {
+            Text("My Story")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            ScrollView {
+                Text(information.story)
+                    .font(.body)
+                    .padding()
+            }
+            ForEach(story[pageIndex].choices, id: \Choice.text) { choice in
+                NavigationLink(destination: StoryPageView(story: story, pageIndex: choice.destination)) {
+                    Text(choice.text)
+                        .multilineTextAlignment(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                        .background(Color.gray.opacity(0.25))
+                        .cornerRadius(8)
+                }
+            }
+        }
+        .padding([.top, .bottom], 50)
+    }
+}
+
+struct StoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView(story: story, pageIndex: 0)
+    }
+}
